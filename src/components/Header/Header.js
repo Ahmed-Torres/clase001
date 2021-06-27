055afa9f4d1d39e19ae7d9afb2560f85ec31e91e
Yamil <torresyamil091@gmail.com>
2021-06-27 16:26:46 -0300

    practicando react

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+const Header = (props) => {
+    console.log(props)
+    return ( 
+        <>
+        <h3>Header 🚀</h3>
+        <p>Esto es mi header</p>
+        </>
+     );
+}
+ 
+export default Header;
